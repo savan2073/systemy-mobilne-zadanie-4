81dac1a78036b7f4ee2840831bddaafe9e228f41
savan2073 <savan2073@gmail.com>
2022-10-22 13:08:42 +0200

    p 24

--- app/src/main/java/pl/edu/pb/todoapp/TaskListFragment.java
+++ app/src/main/java/pl/edu/pb/todoapp/TaskListFragment.java
@@ -1,6 +1,67 @@
 package pl.edu.pb.todoapp;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class TaskListFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.fragment_task_list, container, false);
+        recyclerView = view.findViewById(R.id.task_recycler_view);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return view;
+    }
+
+    private class TaskHolder extends RecyclerView.ViewHolder{
+
+        public TaskHolder(LayoutInflater inflater, ViewGroup parent){
+            super(inflater.inflate(R.layout.list_item_task, parent, false));
+        }
+    }
+
+    private class TaskAdapter extends RecyclerView.Adapter<TaskHolder>{
+        private List<Task> tasks;
+
+        public TaskAdapter(List<Task> tasks){
+            this.tasks = tasks;
+        }
+
+        @NonNull
+        @Override
+        public TaskHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new TaskHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull TaskHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return tasks.size();
+        }
+    }
 }
+
+
